50ef49856ec02b8373ba2bce44ce62313fedd48f
iisquare <iisquare@163.com>
2016-11-11 15:33:51 +0800

    elasticsearch5.x jetty debug

---
+++ elasticsearch5.x/src/test/java/org/elasticsearch/bootstrap/StartElasticsearchSingle.java
@@ -0,0 +1,15 @@
+package org.elasticsearch.bootstrap;
+
+/**
+ * 若程序执行受限，请参照config/java.policy配置${JRE_HOME}/lib/security目录下的同名文件。
+ * @author Ouyang <iisquare@163.com>
+ *
+ */
+public class StartElasticsearchSingle {
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("es.path.home", "server/example/single");
+		Elasticsearch.main(args);
+	}
+	
+}
