a9792a965a11259fe037ac2bdcc9947a5204f85c
fanqingqing <fqq@acewill.cn>
2016-11-13 15:14:43 +0800

    elasticsearch5.x plugin default analysis

--- elasticsearch5.x/src/main/java/com/iisquare/elasticsearch/wltea/lucene/IKAnalyzerProvider.java
+++ elasticsearch5.x/src/main/java/com/iisquare/elasticsearch/wltea/lucene/IKAnalyzerProvider.java
@@ -12,12 +12,12 @@ public class IKAnalyzerProvider extends AbstractIndexAnalyzerProvider<IKAnalyzer
 	public IKAnalyzerProvider(IndexSettings indexSettings, Environment env, String name, Settings settings) {
 		super(indexSettings, name, settings);
 		String dictSerial = settings.get("dictSerial");
-		String useSmart = settings.get("useSmart");
-		String useArabic = settings.get("useArabic");
-		String useEnglish = settings.get("useEnglish");
-		String useSynonym = settings.get("useSynonym");
-		analyzer = new IKAnalyzer(dictSerial.trim(), useSynonym.equals("true"),
-				useSmart.equals("true"), useArabic.equals("true"), useEnglish.equals("true"));
+		String useSmart = settings.get("useSmart", "");
+		String useArabic = settings.get("useArabic", "");
+		String useEnglish = settings.get("useEnglish", "");
+		String useSynonym = settings.get("useSynonym", "");
+		analyzer = new IKAnalyzer(null == dictSerial ? null : dictSerial.trim(),
+				useSynonym.equals("true"), useSmart.equals("true"), useArabic.equals("true"), useEnglish.equals("true"));
 	}
 
 	public static IKAnalyzerProvider enhanceIndexerForIndex(IndexSettings indexSettings, Environment env, String name, Settings settings) {
